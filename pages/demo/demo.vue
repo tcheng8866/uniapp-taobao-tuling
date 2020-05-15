b3407525f636f21db18ca351a58ee235a15390d7
“tianchengcheng” <tian.chengcheng@vpclub.cn>
2020-05-15 09:46:22 +0800

    学习组件

--- pages/demo/demo.vue
+++ pages/demo/demo.vue
@@ -22,7 +22,7 @@
 					{ title: '交易完成', desc: '2018-11-14' }
 				]"
 			></uni-steps>
-			
+						
 			<!-- 宫格: showBorder:显示边框、square:正方形 -->
 			<uni-grid :column="3" :showBorder="true" :square="false">
 				<uni-grid-item><text class="text">文本</text></uni-grid-item>
@@ -40,6 +40,8 @@
 			@change="consoleLog($event)"
 			 />
 
+			<!-- 加冒号的，说明后面的是一个变量或者表达式；没加冒号的后面就是对应的字符串字面量！ -->
+			
 			<!-- 图标: https://ext.dcloud.net.cn/plugin?id=28 -->
 			<uni-icons type="pengyouquan" color="red" size="32" @click="consoleLog($event)"></uni-icons>
 
@@ -52,8 +54,14 @@
 			<!-- 评分： -->
 			<uni-rate value="3.5"  @change="consoleLog($event)" ></uni-rate>
 		
+			<!-- 此处传参必须带冒号是因为、只能接受Number、不带冒号默认字符串 -->
 			<!-- 倒计时: 带天的、不带天的都支持、不传默认0 -->
 			<uni-countdown :day="1" :hour="1" :minute="12" :second="40" :show-day="true"  color="#FFFFFF" background-color="#00B26A" border-color="#00B26A"></uni-countdown>
+			
+			<!-- 加载更多：loading 的状态，可选值：more（loading前）、loading（loading中）、noMore（没有更多了） -->
+			<uni-load-more status="loading"></uni-load-more>
+			<!-- 导航：系统默认就有返回导航 -->
+			<uni-nav-bar left-icon="back" left-text="返回" right-text="菜单" title="导航栏组件"></uni-nav-bar>
 		
 			<!-- 文本相关 -->
 			<!-- 标题:  -->
@@ -63,6 +71,13 @@
 			<uni-card title="标题文字" extra="额外信息" thumbnail="https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png" style="text-align: left">
 				<p>内容主体，可自定义内容及样式</p>
 			</uni-card>
+			
+			<!-- list列表 -->
+			<uni-list>
+			    <uni-list-item title="无箭头" :show-arrow="false"></uni-list-item>
+			    <uni-list-item title="标题文字" note="描述信息" :show-badge="true" badge-text="12"></uni-list-item>
+				<uni-list-item title="标题文字" :show-switch="true" :switchChecked="true" @switchChange="consoleLog($event)" :show-arrow="false"></uni-list-item>
+			</uni-list>
 			
 			<!-- 折叠面板 -->
 			<uni-collapse accordion="true">
@@ -77,17 +92,25 @@
 					</view>
 				</uni-collapse-item>
 			</uni-collapse>
+			
 				
-				<!-- 悬浮按钮: horizontal水平对齐、vertical竖直对齐、direction展开方向（horizontal、vertical） -->
-		        <uni-fab
-		            :content="[
-						{text: 'A', iconPath: '/static/components.png'},
-					]"
-		            horizontal="right"
-		            vertical="bottom"
-		            direction="horizontal"
-		            @trigger="consoleLog($event)"
-		        ></uni-fab>
+			<!-- 悬浮按钮: horizontal水平对齐、vertical竖直对齐、direction展开方向（horizontal、vertical） -->
+			<uni-fab
+				:content="[
+					{text: 'A', iconPath: '/static/components.png'},
+				]"
+				horizontal="right"
+				vertical="bottom"
+				direction="horizontal"
+				@trigger="consoleLog($event)"
+			></uni-fab>
+		
+			<!-- 索引列表 -->
+			<uni-indexed-list 
+			v-if="true"
+			:showSelect="true"
+			:options="options" 
+			@click="consoleLog($event)"></uni-indexed-list>
 		
 		</view>
 	</view>
@@ -100,7 +123,30 @@ export default {
 	components: {},
 	props: {},
 	data() {
-		return {};
+		return {
+			options: [
+				{
+					"letter": "A",
+					"data": [
+						"阿克苏机场",
+						"阿拉山口机场",
+						"阿勒泰机场",
+						"阿里昆莎机场",
+						"安庆天柱山机场",
+						"澳门国际机场"
+					]
+				}, {
+					"letter": "B",
+					"data": [
+						"保山机场",
+						"包头机场",
+						"北海福成机场",
+						"北京南苑机场",
+						"北京首都国际机场"
+					]
+				}
+			]
+		};
 	},
 	computed: {},
 	watch: {},
@@ -136,11 +182,20 @@ uni-image {
 	width: 100%;
 	height: 100%;
 }
-// 修复uni-badge角标不是圆形的Bug
-uni-text {
-	padding: 0 !important;
+// 角标单个数字不是圆形
+/deep/.uni-badge {
+	padding: 0;
 }
+// 卡片标题的图标偏上
 /deep/.uni-card__header-extra-img-view {
 	line-height: 0 ;
+}
+// 评分组件偏上
+/deep/.uni-rate__icon {
+	margin-top: 12px;
+}
+// 索引列表绝对定位
+/deep/.uni-indexed-list {
+	position: relative;
 }
 </style>
